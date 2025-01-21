8c504856b6b0bbc1c05f6ff34941505debd31d89
Jack Woodgate <67034721+lampy255@users.noreply.github.com>
2025-01-21 14:13:01 +1300

    Update types.go

--- types/types.go
+++ types/types.go
@@ -71,6 +71,7 @@ type APIKeyInit struct {
 type ServerInfo struct {
 	PublicKey        string   `json:"publicKey"`
 	PublicEndpoint   string   `json:"publicEndpoint"`
+	PublicHost       string   `json:"publicHost"`
 	NameServers      []string `json:"nameServers"`
 	Netmask          string   `json:"netmask"`
 	ServerInternalIP string   `json:"serverInternalIP"`
